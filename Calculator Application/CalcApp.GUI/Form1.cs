c3a1efb63a9f57d91eb174b755bd22126577848d
herkommer <m@herkommer.co.uk>
2019-10-03 14:20:29 +0200

    initial commit

---
+++ Calculator Application/CalcApp.GUI/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcApp.GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            button1.Text = "+";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //ta de två talen
+            //gör beräkningen "+"
+            //visa svaret i listboxen
+            //hmm..hur kommer jag nu vidare??
+
+            CalculatorEngine calc;
+            calc = new CalculatorEngine();
+
+            int x;
+            int y;
+            x = int.Parse(textBox1.Text);
+            y = int.Parse(textBox2.Text);
+
+            int answer;
+            answer = calc.Add(x, y);
+
+            listBox1.Items.Add(answer);
+        }
+    }
+
+    public class CalculatorEngine
+    {
+        public int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+    }
+}
